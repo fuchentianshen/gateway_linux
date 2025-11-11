b4a45fa69cca8fb36e62ab82978b1c24a7010bf3
wuhan <2749514521@qq.com>
2025-11-11 10:37:05 +0800

    初始化

---
+++ test/make/hello.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+void sayhello(void)
+{
+    printf("hello world\r\n");
+}
---
+++ test/make/main.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+int main(int argc, char const *argv[])
+{
+    sayhello();
+    return 0;
+}
