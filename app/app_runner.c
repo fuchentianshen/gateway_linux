32e26e378695af1779b3fd28d3e740b0e69f9759
wuhan <2749514521@qq.com>
2025-11-17 21:10:16 +0800

    应用运行器模块

--- app/app_runner.c
+++ app/app_runner.c
@@ -1,1 +1,28 @@
 #include "app_runner.h"
+
+static int is_running = 1;
+void exit_handler(int signal)
+{
+    is_running = 0;
+}
+
+void app_runner_run(void)
+{
+    // 注册结束信号处理函数
+    signal(SIGINT, exit_handler);  // 键盘输入Ctrl+C
+    signal(SIGTERM, exit_handler); // kill -15
+
+    // 初始化设备
+    app_device_init(DEVICE_FILE);
+
+    // 启动设备
+    app_device_start();
+
+    while (is_running)
+    {
+        sleep(1);
+    }
+
+    //释放设备
+    app_device_free();
+}
--- app/app_runner.h
+++ app/app_runner.h
@@ -2,5 +2,10 @@
 #define __APP_RUNNER_H__
 
 #include <signal.h>
+#include "app_device.h"
+
+#define DEVICE_FILE "/dev/ttyS1"
+
+void app_runner_run(void);
 
 #endif /* __APP_RUNNER_H__ */
